741dcb2256ee9e182e1dffc234a7a1927a69ec22
rushikeshnarule <75885428+rushikeshnarule@users.noreply.github.com>
2023-08-28 19:25:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'shareus.io')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'api.shareus.io')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'poys3CaHcCSlg1dPNi5S2N4Qrgi1')
 
      # Auto Delete For Group Message (Self Delete) #
